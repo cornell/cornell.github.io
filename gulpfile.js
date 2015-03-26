44b72603dee8da722ac19c7084427abad2c15b91
cornell <SMazurais@SYFADIS55.CONDATE.com>
2015-03-26 18:32:16 +0100

    mise à jour du fichier gulp + ajout de jshint et jssc

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,58 @@
+"use strict";
+
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var stylish = require('jshint-stylish');
 var concat = require('gulp-concat');
+var browserSync = require('browser-sync');
 
-gulp.task('concat', function(){
-});
+//gulp.task('concat', function(){
+//    
+//});
 
-gulp.tasj('js-hint', function(){
-    gulp.src('.js/*.js')
-        .pipe()
-});
+//gulp.task('js-hint', function(){
+//    gulp.src('.js/*.js')
+//        .pipe()
+//});
 
 gulp.task('watch', function(){
     gulp.watch('js/*.js', ['jshint']);
-    gulp.watch('', [])
-})
+    gulp.watch('', []);
+});
+
+gulp.task('browser-sync', function () {
+   var files = ["index.html",
+        "css/index.css",
+        "js/*.js"
+    ];
+
+   browserSync.init(files, {
+      server: {
+         baseDir: '.'
+      }
+   });
+});
+
+gulp.task('build-css', function(){
+    
+    gulp.src([
+        'vendor/mustache/mustache.min.js', 
+        'js/app.js'
+    ])
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter(stylish))
+    .pipe(jshint.reporter('fail'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('lint-js', function(){
+    
+    gulp.src([         
+        'js/app.js'
+    ])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['lint-js']);
