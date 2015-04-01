098dfcfadc62426102820d68b3c63fe2dd86c929
cornell <SMazurais@SYFADIS55.CONDATE.com>
2015-04-01 18:05:31 +0200

    modification de gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 "use strict";
 
 var gulp = require('gulp');
+var debug = require('gulp-debug');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var browserSync = require('browser-sync');
 var rev = require('gulp-rev');
+var useref= require('gulp-useref');
+var del = require('del');
 
 //gulp.task('concat', function(){
 //    
@@ -61,8 +64,33 @@ gulp.task('build', function () {
     gulp.src([
         'src/js/app.js'
     ])
+    .pipe(debug({title: "beforeRev"}))
     .pipe(rev())
+    .pipe(debug({title: "AfterRev"}))
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('useref', ['clean:dist'], function () {
+    var assets = useref.assets();
+    
+    return gulp.src('src/index.html')
+        .pipe(debug({title: "before assets"}))
+        .pipe(assets)
+        .pipe(debug({title: "before assets restore"}))
+        .pipe(assets.restore())
+        .pipe(debug({title: "before useref"}))
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clean:dist', function(cb){
+  del([
+    'dist/index.html',
+    // here we use a globbing pattern to match everything inside the `mobile` folder
+    'dist/**',
+    // we don't want to clean this file though so we negate the pattern
+    '!dist/deploy.json'
+  ], cb);
 });
 
 gulp.task('default', ['build']);
