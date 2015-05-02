092ee982bf5ecc8024f9f8bdfbb970a84a8f2b3f
cornell <cornell@netcourrier.com>
2015-05-02 14:00:09 +0200

    Ajout history

--- src/js/app.js
+++ src/js/app.js
@@ -1,45 +1,4 @@
-module.exports = function(global, movies){
-    "use strict";
+(function (params) {
     
-    var tickets = [];
-
-    global.showMovies = function(){
-
-        movies.forEach(function(m){
-            console.log(m.id + ') ' + m.name + ' | $' + m.price.toFixed(2));
-        });
-    };
-
-    global.buyTicket = function(id){
-
-        id = id || 0;
-        id -= 1;
-
-        if(movies[id] !== undefined){
-            tickets.push(id);
-            console.log('You bought a ticket for "' + movies[id].name + '"!');
-        }
-        else{
-            console.error('No such movie!');
-        }
-    };
-
-    global.showTickets = function(){
-
-        tickets.forEach(function(id){
-            console.log('Ticket for "' + movies[id].name + '" | $' + movies[id].price.toFixed(2));
-        });
-
-    };
-
-    global.totalCost = function(){
-
-        var total = 0;
-
-        tickets.forEach(function(id){
-            total += movies[id].price;
-        });
-
-        console.log('You have to pay $' + total.toFixed(2));
-    };
-};
+    
+})();
